9fc06c33755ab7ffb26996ce3b6d55de3f1a3510
Youni Verciti <oliv71@laposte.net>
2016-12-16 17:27:38 -0200

    Rev 1.3.6.1 Clean rch.py
    Change-Id: I133782d8e5b2365c724a905d90230c8caad0bc2d

--- rch.py
+++ rch.py
@@ -21,40 +21,16 @@ def find_content(page): # reçoit un objet page retourne l'objet sous-page/Conte
 def rch_labo(): 
   category_title = u'fr:Catégorie:Laboratoire de recherche' # à partir de la catégorie
   page = pywikibot.page.Category(site, category_title)      # créé la l'objet page.cat PWB
-  articles = page.articles()       # collecte liste des articles de la catégorie
-  articles = list(articles) # convertit en liste
-  for article in articles:  # chaque article de la catégorie
+  articles = page.articles() # collecte liste des articles de la catégorie
+  articles = list(articles)  # convertit en liste
+  for article in articles:   # chaque article de la catégorie
     l_doc = check_link_in_subpage(article, '/Contenu', ns_id)
-    # VOIR find_content()
-    #page = find_content(article) # objet page pour la sous-page Contenu
-    #exist = page.exists()        # test si trouve /Contenu
-    #if exist: 
-    #  gen = get_linked_p(page, 104) # collecte les liens sur cette page vers ns Recherche
-    #  l_gen = list(gen)        # MIEUX convertit le gen en liste
     for mypage in dict_page: # 
       if mypage == article:
 	page_prop = dict_page[mypage]
-	page_prop['l_doc'] = l_gen   # place la liste dans le dictionnaire des propriétés
+	page_prop['l_doc'] = l_doc   # place la liste dans le dictionnaire des propriétés
+	page_prop['n_doc'] = len(l_doc)
 	page_prop['class_doc'] = '\'lab\'' # NOUVELLE VARIABLE
-  return dict_page
-def rch_labo_old(): 
-  category_title = u'fr:Catégorie:Laboratoire de recherche' # à partir de la catégorie
-  page = pywikibot.page.Category(site, category_title)      # créé la l'objet page.cat PWB
-  articles = page.articles()       # collecte liste des articles de la catégorie
-  articles = list(articles) # convertit en liste
-  for article in articles:  # chaque article de la catégorie
-    # VOIR check_link_in_subpage(page, '/subpage', ns_id)
-    # VOIR find_content()
-    page = find_content(article) # objet page pour la sous-page Contenu
-    exist = page.exists()        # test si trouve /Contenu
-    if exist: 
-      gen = get_linked_p(page, 104) # collecte les liens sur cette page vers ns Recherche
-      l_gen = list(gen)        # MIEUX convertit le gen en liste
-      for mypage in dict_page: # 
-	if mypage == article:
-	  page_prop = dict_page[mypage]
-	  page_prop['l_doc'] = l_gen   # place la liste dans le dictionnaire des propriétés
-	  page_prop['class_doc'] = '\'lab\'' # NOUVELLE VARIABLE
   return dict_page
 
 ### VARIABLES
@@ -82,17 +58,17 @@ for page in dict_page:  # Analyse des départements de recherche
   prefix = "fr:Recherche:Département:" # prefix des departement de recherche
   s = str(page)
   if prefix in s and page_prop['nsep'] == 0: # si departement de recherche
-    # VOIR check_link_in_subpage(page, '/subpage', ns_id)
-    subp = find_content(page) # Objet pour la sous-page "/Contenu"
-    exist = subp.exists()     # test si trouve /Contenu
-    if exist:
-      gen = get_linked_p(subp, 104) # titre de la page et namespace id
-      l_gen = list(gen)
-      page_prop['class_doc'] = '\'dptr\'' # NOUVELLE VARIABLE
-      page_prop['l_doc'] = l_gen            # place la liste dans le dictionnaire des propriétés
-  #ATTENTION les liens sont sur la sous-page /Contenu 
-  # ATTENTION les liens sont sur la sous-page /Contenu mais nous les affectons aux propriétés
-      # de la page du departement (page supérieure ou racine)
+    l_doc = check_link_in_subpage(page, '/Contenu', ns_id)
+    #subp  = find_content(page) # Objet pour la sous-page "/Contenu"
+    #exist = subp.exists()     # test si trouve /Contenu
+    #if exist:
+    #  gen = get_linked_p(subp, 104) # titre de la page et namespace id
+    #  l_gen = list(gen)
+    page_prop['l_doc'] = l_doc       # place la liste dans le dictionnaire des propriétés
+    page_prop['n_doc'] = len(l_doc)
+    page_prop['class_doc'] = '\'dptr\'' # NOUVELLE VARIABLE
+    
+# ATTENTION les liens sont sur la sous-page /Contenu mais nous les affectons aux propriétés de la page du departement (page supérieure ou racine)
 
 dict_page = rch_labo()
 
@@ -104,3 +80,23 @@ lua_code = table_prop_code + table_pages_code  # Concatener le code Lua
 module_name = u'ns_' + nsdata['label']  # enregistre le module du namespace
 #print lua_code                         # TEST affiche le code du module
 write_module_lua(module_name, lua_code) # Ecriture du module #TEST 
+
+#def rch_labo_old(): 
+  #category_title = u'fr:Catégorie:Laboratoire de recherche' # à partir de la catégorie
+  #page = pywikibot.page.Category(site, category_title)      # créé la l'objet page.cat PWB
+  #articles = page.articles()       # collecte liste des articles de la catégorie
+  #articles = list(articles) # convertit en liste
+  #for article in articles:  # chaque article de la catégorie
+    ## VOIR check_link_in_subpage(page, '/subpage', ns_id)
+    ## VOIR find_content()
+    #page = find_content(article) # objet page pour la sous-page Contenu
+    #exist = page.exists()        # test si trouve /Contenu
+    #if exist: 
+      #gen = get_linked_p(page, 104) # collecte les liens sur cette page vers ns Recherche
+      #l_gen = list(gen)        # MIEUX convertit le gen en liste
+      #for mypage in dict_page: # 
+	#if mypage == article:
+	  #page_prop = dict_page[mypage]
+	  #page_prop['l_doc'] = l_gen   # place la liste dans le dictionnaire des propriétés
+	  #page_prop['class_doc'] = '\'lab\'' # NOUVELLE VARIABLE
+  #return dict_page
