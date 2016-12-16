dc175b6807a9687f088924a19d028ce10bb577bb
Youni Verciti <oliv71@laposte.net>
2016-12-16 11:42:54 -0200

    Rev 1.3.5.95 wkt.py
    Change-Id: I305d23decf34b117b06840c2c10acc5331d27fd4

--- wkt.py
+++ wkt.py
@@ -16,11 +16,15 @@ nsdata = ns_collect_data(ns_id)           # Scan l'espace de noms VERSION 2
 dict_page = nsdata['dict_page']
 
 ## Collection des dates de 1ère révision
-prefix_list = [u'La salle café', u'Pages à supprimer', u'Administrateur/Candidature', u'Bureaucrate/Candidature]',] # UNICODE
+prefix_list = [u'La salle café', 
+	       u'Pages à supprimer', 
+	       u'Administrateur/Candidature', 
+	       u'Bureaucrate/Candidature]',] # UNICODE
+
 ns_get_date(dict_page, prefix_list, nsdata['label'])  # dates de 1ère révision
   
 table_prop_code = wlms_table_prop(ns_id, nsdata) # la table des propriétés de l'espace de noms ¿ns_id?
-table_pages_code = wlms_table_pages(dict_page)   # la table des pages
+table_pages_code = wlms_table(dict_page, 'pages')   # la table des pages
 # Concatener le code Lua ici
 lua_code = table_prop_code + table_pages_code  # Concatener le code Lua
 
