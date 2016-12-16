a749419de0ed714b10dc277d4b6e18a3da93e0b1
Youni Verciti <oliv71@laposte.net>
2016-12-16 13:03:46 -0200

    Rev 1.3.5.98 trw.py clean
    Change-Id: I069e82c518da2341f78157da06c2016a6a6a9a94

--- rch.py
+++ rch.py
@@ -6,13 +6,9 @@ from namespace_lib import *
 from lua_mw_lib import *
 import pywikibot 
 
-lang = 'fr'       
-family = 'wikiversity'
-site = pywikibot.Site(lang, family)  
-ns_id = 104   
-
 ### FONCTIONS NS 104
-### Liste des documents de recherche liés dans les Laboratoires de recherche
+
+### Liste des documents de recherche liés dans la sous-page "/Contenu"
 #   Charger la liste des pages à analyser via celle des articles dans la catégorie Laboratoire de recherche
 def find_content(page): # reçoit un objet page retourne l'objet sous-page/Contenu
   spage = str(page)
@@ -25,7 +21,7 @@ def rch_labo():
   category_title = u'fr:Catégorie:Laboratoire de recherche' # à partir de la catégorie
   page = pywikibot.page.Category(site, category_title)      # créé la l'objet page.cat PWB
   articles = page.articles()       # collecte liste des articles de la catégorie
-  articles = gen_to_list(articles) # MIEUX : articles = list(articles)
+  articles = list(articles) # MIEUX : articles = list(articles)
   for article in articles:  # chaque article de la catégorie
     page = find_content(article) # objet page pour la sous-page Contenu
     exist = page.exists()        # test si trouve /Contenu
@@ -41,6 +37,12 @@ def rch_labo():
       # de la page du departement (page supérieure ou racine)
   return dict_page
 
+### VARIABLES
+lang = 'fr'       
+family = 'wikiversity'
+site = pywikibot.Site(lang, family)  
+ns_id = 104   
+
 ### ETAPE 1 
 nsdata = ns_collect_data(ns_id)           # Scan l'espace de noms VERSION 2
 dict_page = nsdata['dict_page']
@@ -49,6 +51,7 @@ dict_page = nsdata['dict_page']
 ### Ajoute la liste des doc de recherche au dictionnaire des pages
 #   Filtre les departements de recherche
 ### Collecte les liens vers ns_104 retourne la liste l_doc
+### ADAPTER FX rch_labo():
 for page in dict_page:  # Analyse des départements de recherche
   page_prop = dict_page[page]
   prefix = "fr:Recherche:Département:" # prefix des departement de recherche
@@ -58,15 +61,15 @@ for page in dict_page:  # Analyse des départements de recherche
     exist = subp.exists()     # test si trouve /Contenu
     if exist:
       gen = get_linked_p(subp, 104) # titre de la page et namespace id
-      gen = gen_to_list(gen)
+      l_gen = list(gen)
       page_prop['class_doc'] = '\'dptr\'' # NOUVELLE VARIABLE
-      page_prop['l_doc'] = gen            # place la liste dans le dictionnaire des propriétés
+      page_prop['l_doc'] = l_gen            # place la liste dans le dictionnaire des propriétés
   #ATTENTION les liens sont sur la sous-page /Contenu 
 
 dict_page = rch_labo()
 
-table_prop_code = wlms_table_prop(ns_id, nsdata) # la table des propriétés de l'espace de noms
-table_pages_code = wlms_table_pages(dict_page) ###TEST wlms_table_pages(nsdata)       # la table des pages
+table_prop_code = wlms_table_prop(ns_id, nsdata)  # la table des propriétés de l'espace de noms
+table_pages_code = wlms_table(dict_page, 'pages') # TEST wlms_table() 
 # Concatener le code Lua ici
 lua_code = table_prop_code + table_pages_code  # Concatener le code Lua
 
--- trw.py
+++ trw.py
@@ -16,7 +16,7 @@ nsdata = ns_collect_data(ns_id) # Scan l'espace de noms collecte les données
 dict_page = nsdata['dict_page'] # reconnait le dictionnaire des pages
 
 table_prop_code = wlms_table_prop(ns_id, nsdata) # la table des propriétés de l'espace de noms
-table_pages_code = wlms_table_pages(dict_page)   # la table des pages
+table_pages_code = wlms_table(dict_page, 'pages')   # la table des pages
 # Concatener le code Lua ici
 lua_code = table_prop_code + table_pages_code  # Concatener le code Lua
 
