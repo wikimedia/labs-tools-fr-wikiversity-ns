af8dea0c85e951157bc77abf76cbf7bd85187818
Youni Verciti <oliv71@laposte.net>
2016-12-24 11:13:00 -0200

    Rev 1.4.0 Talk spaces monitoring begin in hlp.py
    Change-Id: I4b1ff43453bf0a483b0749e27f42ac8c70af47b6

--- hlp.py
+++ hlp.py
@@ -11,22 +11,25 @@ family = 'wikiversity'
 site = pywikibot.Site(lang, family)  
 ns_id = 12   # identifiant namespace
 
-### Collect data 
+### Collect data MAIN SPACE
 nsdata = ns_collect_data(ns_id)           # Scan l'espace de noms VERSION 2
 dict_page = nsdata['dict_page']
+### Collect data DISCUSSIONS NAMESPACES
+ns_talk_id = ns_id + 1
+nstalk = ns_collect_data(ns_talk_id)  # Scan l'espace de noms VERSION 2
+dict_page = nstalk['dict_page']       # ATTENTION nom du dictionnaire immuable
 
 ### Write Lua tables
-table_prop_code = wlms_table_prop(ns_id, nsdata)   # la table des propriétés de l'espace de noms
-table_pages_code =  wlms_table(dict_page, 'pages') # la table des pages ATTENTION definit variable
-# Concatener le code Lua ici
-lua_code = table_prop_code + table_pages_code      # Concatener le code Lua
-### Save Module
-module_name = u'ns_' + nsdata['label']  # enregistre le module du namespace
-#print lua_code                         # TEST affiche le code du module
-write_module_lua(module_name, lua_code) # Ecriture du module #TEST 
-
-### DISCUSSIONS NAMESPACES
-# ns_talk_id = ns_id + 1
-# nstalk = ns_collect_data(ns_talk_id)  # Scan l'espace de noms VERSION 2
-# dict_talk_page = nstalk['dict_talk_page']
-
+table_prop_code  = wlms_table_prop(ns_id, nsdata)  # la table des propriétés de l'espace de noms
+table_pages_code = wlms_table(dict_page, 'pages')  # la table des pages
+lua_main_code    = table_prop_code + table_pages_code # Concatener le code Lua
+main_module_name = u'ns_' + nsdata['label']  # enregistre le module de l'espace de noms
+#   Talk tables
+talk_prop_code   = wlms_table_prop(ns_id, nstalk)     # la table des propriétés de l'espace discussion
+talk_pages_code  = wlms_table(dict_page, 'talkpages') # la table des pages de discussion
+lua_talk_code    = talk_prop_code + talk_pages_code   # Concatener le code Lua
+talk_module_name = u'ns_' + nstalk['label']  # enregistre le module de l'espace discussion relatif
+#   Save Modules
+write_module_lua(main_module_name, lua_main_code) # Ecriture du module #TEST 
+write_module_lua(talk_module_name, lua_talk_code) # Ecriture des tables de l'espace discussion
+#print lua_main_code                         # TEST affiche le code du module
--- talktest.py
+++ talktest.py
@@ -21,13 +21,14 @@ nstalk = ns_collect_data(ns_talk_id)  # Scan l'espace de noms VERSION 2
 dict_page = nstalk['dict_page']       # ATTENTION nom du dictionnaire immuable
 
 ### Write Lua tables
-table_prop_code = wlms_table_prop(ns_id, nstalk)   # la table des propriétés de l'espace de noms
-table_pages_code =  wlms_table(dict_page, 'talkpages') # la table des pages ATTENTION definit variable
+talk_prop_code = wlms_table_prop(ns_id, nstalk)       # la table des propriétés de l'espace discussion
+talk_pages_code =  wlms_table(dict_page, 'talkpages') # la table des pages de discussion
 # Concatener le code Lua ici
-lua_code = table_prop_code + table_pages_code      # Concatener le code Lua
+lua_code = talk_prop_code + talk_pages_code      # Concatener le code Lua
 ### Save Module
-module_name = u'ns_' + nstalk['label']   # enregistrer une table dans le module de l'espace relatif
+talk_module = u'ns_' + nstalk['label']   # enregistrer une table dans le module de l'espace relatif
 print lua_code                           # TEST affiche le code du module
 #print module_name
 #print comment
-#write_module_lua(module_name, lua_code) # Pas un nouveau module seulement une nlle table dans le module d'origine
+write_module_lua(talk_module, lua_code)
+# # Pas un nouveau module seulement une nlle table dans le module d'origine
