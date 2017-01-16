57e3759d54b630aff78ecf2ce1ca7f00966f0d37
Youni Verciti <oliv71@laposte.net>
2017-01-16 19:32:38 -0200

    Rev 1.5.0.1 Lower caser for module name in  hlp.py
    Change-Id: I7a4dbf30df439b1020b6b795cce1d913930a0cf9

--- lua_mw_lib.py
+++ lua_mw_lib.py
@@ -75,32 +75,6 @@ def wmls_table_input(page_prop):
   t = t + wmls_table_next  # ferme la table ajoute une virgule
   return t
 
-#def wmls_table_input_SAVE_OLD(page_prop):
-  #t = ''
-  #for prop in page_prop:
-    #if page_prop[prop] == '':  # si la valeur est vide
-      #pass                     # Ne code pas le paramètre 
-        ##t = t + str(prop) + ' = \'\', '    # formate le code lua
-    #if type(page_prop[prop]) == list:
-      #t = t + str(prop) + ' = ' + wmls_list_to_lua(page_prop[prop])
-    #elif prop == 'date1': # AMELIORER type int on reconnait la date par le nom du param.
-        #t = t + str(prop) + ' = \'' + str(page_prop[prop]) + '\', ' # formate le code lua
-    #elif type(page_prop[prop]) == dict :   # type = DICT #print type(page_prop[i])
-        ## sous-fonction ; ici on sait que le dict contient des listes
-        ## il faudrait tester le type de chaque valeur autre sous-fonction
-        #mydict = page_prop[prop]
-        #for key in mydict:
-          #v = mydict[key]
-          ## Rustine pour d_lessons AMéliorer :
-          #if type(v) == int :
-            #t = t + str(key) + ' = ' + str(v) + ', ' # Ce sont des entiers
-          #if type(v) == list :
-            #t = t + str(key) + ' = ' + wmls_list_to_lua(v)   # Ce sont des listes
-    #else:
-      #t = t + str(prop) + ' = ' + str(page_prop[prop]) + ', '  # formate k, v
-  #t = t + wmls_table_next  # ferme la table ajoute une virgule
-  #return t
-
 ### Construit la table Lua t_prop des propriétés de l'espce de noms
 #   exclu le dictionnaire des pages
 def wlms_table_prop(ns_id, nsdata): # 
@@ -128,15 +102,3 @@ def wmls_list_to_lua2(l): # convertit une liste python en table Lua
   code = code + '} \n'           # ferme la table + saut de ligne (SANS VIRGULE FINALE)
   return code                     # retourne le code Lua
 
-####DOUBLE EMPLOI avec wlms_table(python_dict, table_name)
-##  REMPLACER PAR wlms_table(dict_page, 'pages') -> hlp, cat
-## ATTENTION OFF
-#def wlms_table_pages(dict_page): 
-  #t = 'p.t_pages = {\n'  # placer la chaine dans une variable
-  #for page in dict_page: # pour chaque page
-    #t = t + '{page = ' + str(page) + ', ' # compile clé et valeur
-    #page_prop = dict_page[page]           # le dictionnaire des proriétés de la page
-    #t = t + wmls_table_input(page_prop)   # comple le code Lua des propriétés
-  #t = t + wmls_table_close                # fin de table
-  #t = unicode(t, 'utf_8')                 # UNICODE
-  #return t
